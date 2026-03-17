d880a1ccc08f6043380cee7f79ca9243870c095c
SabnamJuhi <sjuhisarkars95@gmail.com>
2026-03-17 12:14:29 +0530

    changed filter

--- src/controllers/searchService/searchService.controller.js
+++ src/controllers/searchService/searchService.controller.js
@@ -1,61 +1,3 @@
-// const { Op } = require("sequelize");
-
-// const Product = require("../../models/products/product.model");
-// const Category = require("../../models/category/category.model");
-// const SubCategory = require("../../models/category/subcategory.model");
-// const ProductCategory = require("../../models/category/productCategory.model");
-
-// exports.searchProducts = async (req, res) => {
-//   try {
-//     const { keyword } = req.query;
-
-//     if (!keyword) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Search keyword is required",
-//       });
-//     }
-
-//     const products = await Product.findAll({
-//       where: {
-//         [Op.or]: [
-//           { title: { [Op.iLike]: `%${keyword}%` } },
-//           { "$Category.name$": { [Op.iLike]: `%${keyword}%` } },
-//           { "$SubCategory.name$": { [Op.iLike]: `%${keyword}%` } },
-//           { "$ProductCategory.name$": { [Op.iLike]: `%${keyword}%` } },
-//         ],
-//       },
-//       include: [
-//         {
-//           model: Category,
-//           attributes: ["id", "name"],
-//         },
-//         {
-//           model: SubCategory,
-//           attributes: ["id", "name"],
-//         },
-//         {
-//           model: ProductCategory,
-//           attributes: ["id", "name"],
-//         },
-//       ],
-//       distinct: true,
-//     });
-
-//     res.status(200).json({
-//       success: true,
-//       count: products.length,
-//       data: products,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({
-//       success: false,
-//       message: "Search failed",
-//     });
-//   }
-// };
-
 const { Op } = require("sequelize");
 
 const Product = require("../../models/products/product.model");
@@ -155,10 +97,6 @@ exports.searchProducts = async (req, res) => {
           attributes: ["id", "name"],
         },
         {
-          model: ProductPrice,
-          as: "price",
-        },
-        {
           model: ProductSpec,
           as: "specs",
         },
@@ -176,7 +114,11 @@ exports.searchProducts = async (req, res) => {
             {
               model: VariantSize,
               as: "sizes",
-              attributes: ["id", "diameter", "length", "stock"],
+              attributes: ["id", "diameter", "length", "stock", "approxWeightKg"],
+            },
+            {
+              model: ProductPrice,
+              as: "price",
             },
           ],
         },
