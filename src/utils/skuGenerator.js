f03900aaa35341be8d7a58a485f3e1daabe37cc7
SabnamJuhi <sjuhisarkars95@gmail.com>
2026-04-18 18:41:30 +0530

    sfshjwkj

--- src/utils/skuGenerator.js
+++ src/utils/skuGenerator.js
@@ -1,60 +1,7 @@
 
-// const VariantSize = require("../models/productVariants/variantSize.model");
-// const ProductVariant = require("../models/productVariants/productVariant.model");
-
-// async function generateSKU(product, transaction) {
-
-//   /* ---- CATEGORY CODE ---- */
-//   const categoryCode =
-//     product.Category?.name?.substring(0, 2).toUpperCase() || "NA";
-
-//   const subCategoryCode =
-//     product.SubCategory?.name?.substring(0, 4).toUpperCase() || "NA";
-
-//   const productCategoryCode =
-//     product.ProductCategory?.name?.substring(0, 2).toUpperCase() || "NA";
-
-
-//   /* ---- FIRST VARIANT ---- */
-//   const variant = await ProductVariant.findOne({
-//     where: { productId: product.id },
-//     order: [["createdAt", "ASC"]],
-//     transaction,
-//   });
-
-//   const grade = variant?.grade || "00";
-//   const packQty = variant?.packQuantity || "1";
-
-
-//   /* ---- FIRST SIZE ---- */
-//   const size = await VariantSize.findOne({
-//     where: { variantId: variant?.id },
-//     order: [["createdAt", "ASC"]],
-//     transaction,
-//   });
-
-//   const diameter = size?.diameter || "0";
-//   const length = size?.length || "0";
-
-
-//   /* ---- FINAL SKU ---- */
-//   const sku = `SK-${categoryCode}-${subCategoryCode}-${productCategoryCode}-${grade}-M${diameter}-${length}-${packQty}-${Date.now()}`;
-
-//   return sku;
-// }
-
-// module.exports = generateSKU;
-
-
-// SK-{CAT}-{SUB}-{PROD}-{VARIANT}-{MEASUREMENTS}-{MOQ}-{TS}
-
-
-
-
-
 
 const ProductVariant = require("../models/productVariants/productVariant.model");
-const ProductMeasurement = require("../models/products/ProductMeasurement.model");
+const ProductMeasurement = require("../models/products/productMeasurement.model");
 
 async function generateSKU(product, transaction) {
 
